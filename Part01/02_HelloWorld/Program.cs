774940c4453d76415a4e68d7ff420ad37a5e811d
JungSeongKwon <Jungsam000@gmail.com>
2024-10-24 20:57:21 +0900

    1024_Upload
    + Day 1 (1 ~ 3, 14m)

---
+++ Part01/02_HelloWorld/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+// 02 비주얼스튜디오 설치하고 출력
+namespace _02_HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ Part01/03_Console/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+
+// 03 콘솔에서 읽고 쓰기
+namespace _03_Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Hello ");
+            Console.WriteLine("World!");
+            Console.Write("이름을 입력하세요 : ");
+
+            string name = Console.ReadLine();
+            Console.Write("안녕하세요, ");
+            Console.Write(name);
+            Console.WriteLine("님!");
+        }
+    }
+}
